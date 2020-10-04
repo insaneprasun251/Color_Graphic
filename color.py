eee7903a9aeb885daeaf3d17398d9a8d3e5ea987
insaneprasun251 <72152148+insaneprasun251@users.noreply.github.com>
2020-10-04 11:25:44 +0530

    Update color.py

--- color.py
+++ color.py
@@ -3,12 +3,18 @@ import turtle
 
 colors = ['red','purple','blue','green','orange','yellow']
 t = turtle.Pen()
-t.speed(10)
+t.speed(100)
 turtle.bgcolor("Black")
 for x in range(360):
 	t.pencolor(colors[x%6])
 	t.width(x/100+1)
 	t.forward(x)
+	t.left(59)
+
+for i in range(360,0,-1):
+	t.pencolor(colors[i%6])
+	t.width(i/100+1)
+	t.forward(i)
 	t.left(59)
 
 
